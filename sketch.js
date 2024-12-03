4a6566508490d9b5f1b2052e060e2aa2d5869e59
parenzo420 <lorenzo3.gerosa@mail.polimi.it>
2024-12-03 15:22:59 +0100

    Test-esagono.js

--- sketch.js
+++ sketch.js
@@ -1,17 +1,53 @@
-function preload() {
-  // put preload code here
-}
+let hexagonSize;
+let titleText = "Titolo";
+let displayedText = "";
+let lastTypewriterUpdate = 0;
+let typewriterSpeed = 100; // milliseconds per character
 
 function setup() {
   createCanvas(windowWidth, windowHeight);
-  // put setup code here
-  const message =
-    "This is a template repository\nfor the course Laboratorio di Computergrafica\nCommunication Design, Politecnico di Milano";
+  hexagonSize = min(width, height) / 4;
+  
   textAlign(CENTER, CENTER);
-  textSize(16);
-  text(message, width / 2, height / 2);
+  textSize(50);
 }
 
 function draw() {
-  // put drawing code here
+  background(30);
+  
+  updateTypewriter();
+  noStroke();
+  fill(250);
+  text(displayedText, width / 2, height * 0.4 - hexagonSize - 40);
+  
+  fill(150);
+  stroke(50)
+  strokeWeight(5);
+  drawHexagon(width / 2, height / 2, hexagonSize);
+}
+
+function drawHexagon(x, y, size) {
+  beginShape();
+  for (let i = 0; i < 6; i++) {
+    // per mettere la base dell'esagono in verticale:
+    let angle = TWO_PI / 6 * i - TWO_PI / 4;
+    let vx = x + cos(angle) * size;
+    let vy = y + sin(angle) * size;
+    vertex(vx, vy);
+  }
+  endShape(CLOSE);
+}
+
+function updateTypewriter() {
+  if (millis() - lastTypewriterUpdate > typewriterSpeed) {
+    if (displayedText.length < titleText.length) {
+      displayedText += titleText[displayedText.length];
+      lastTypewriterUpdate = millis();
+    }
+  }
+}
+
+function windowResized() {
+  resizeCanvas(windowWidth, windowHeight);
+  hexagonSize = min(width, height) / 4;
 }
